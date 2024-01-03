b4284737a4ad117e6e22f6452776f25f33853286
milka <119007476+milkafasikag@users.noreply.github.com>
2024-01-03 12:59:18 +0300

    Add files via upload

---
+++ test/unit_tests/local_provider_test.dart
@@ -0,0 +1,15 @@
+// import 'package:test/test.dart';
+
+// import 'package:task_management/models/models.dart';
+// import 'package:task_management/data_providers/local_task_data_provider.dart';
+
+// void main() {
+//   test("description", () {
+//     Task task = Task(
+//         title: "Do this fast",
+//         notes: "I would like to get this done real quick.",
+//         isDone: false,
+//         isImportant: false,
+//         category: "School");
+//   });
+// }
